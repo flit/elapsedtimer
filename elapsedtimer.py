490f28b4a9102a3f976de7d6c8c63e4f0372982a
Chris Reed <flit@me.com>
2014-12-06 14:28:31 -0600

    Various improvements.
    - Moved units table to module level.
    - Renamed format_time() to format_duration().
    - Added optional output file param to ElapsedTimer constructor.
    - Refactored __enter__ and __exit__ implementation into other methods,
      including a new start() and stop().
    - Added properties for a local enable, elapsed time, and a property to
      get the elapsed time as a datetime.timedelta object.

--- elapsedtimer.py
+++ elapsedtimer.py
@@ -3,7 +3,9 @@
 
 from __future__ import print_function
 import time
+import datetime
 import sys
+import logging
 
 __all__ = ['ElapsedTimer']
 
@@ -16,25 +18,33 @@ if sys.platform == 'win32':
 else:
     hires_clock = time.time
 
+## Table of time units.
+#
+# List elements are a 3-tuple containing:
+# - Cutoff value for this entry. The entry should be used if the time value is less
+#   than this cutoff.
+# - Unit string.
+# - Multiplier to convert the time value to this entry's units.
+units_table = [   (1e-9, u'ps', 1e12),
+            (1e-6, u'ns', 1e9),
+            (1e-3, u'µs', 1e6),
+            (1.0,  u'ms', 1e3),
+            (60,   u's',  1.0),
+            (60*60, u'min', 1.0/60),
+            (24*60*60, u'hrs', 1.0/(60*60)),
+            (0, u's', 1.0)  ]   # Revert to seconds if very large
+
 def get_time_units_and_multiplier(seconds):
     """
     Given a time in seconds, determines the best units and multiplier to
     use to display the time. Return value is a 2-tuple of units and multiplier.
     """
-    table = [   (1e-9, u'ps', 1e12),
-                (1e-6, u'ns', 1e9),
-                (1e-3, u'µs', 1e6),
-                (1.0,  u'ms', 1e3),
-                (60,   u's',  1.0),
-                (60*60, u'min', 1.0/60),
-                (24*60*60, u'hrs', 1.0/(60*60)),
-                (0, u's', 1.0)  ]   # Revert to seconds if very large
-    for cutoff, units, multiplier in table:
+    for cutoff, units, multiplier in units_table:
         if seconds < cutoff:
             break
     return units, multiplier
 
-def format_time(seconds):
+def format_duration(seconds):
     """Formats a number of seconds with the best units."""
     units, divider = get_time_units_and_multiplier(seconds)
     seconds *= divider
@@ -47,21 +57,54 @@ class ElapsedTimer(object):
     exits, a message will be printed to stdout with the elapsed time and the
     task description.
     """
-    def __init__(self, task=''):
+    def __init__(self, task='', output=sys.stdout):
         self._task = task
+        self._start = 0
+        self._end = 0
+        self._delta = 0
+        self._enable = True
+        self._file = output
 
     def __enter__(self):
-        self._start = hires_clock()
+        self.start()
 
     def __exit__(self, type, value, traceback):
+        self.stop()
+        self._print_message()
+
+    def start(self):
+        self._start = hires_clock()
+
+    def stop(self):
         self._end = hires_clock()
         self._delta = self._end - self._start
 
+    @property
+    def enable(self):
+        return self._enable
+
+    @enable.setter
+    def enable(self, value):
+        self._enable = value
+
+    @property
+    def elapsed(self):
+        if self._end:
+            return self._delta
+        else:
+            return hires_clock() - self._start
+
+    @property
+    def timedelta(self):
+        return datetime.timedelta(seconds=self.elapsed)
+
+    def _print_message(self):
         global enable
-        if enable:
-            displayDelta = format_time(self._delta)
+        if enable and self._enable:
+            displayDelta = format_duration(self._delta)
             if len(self._task):
-                print("%s: %s" % (displayDelta, self._task))
+                msg = "%s: %s" % (displayDelta, self._task)
             else:
-                print(displayDelta)
+                msg = displayDelta
+            self._file.write(msg)
 
